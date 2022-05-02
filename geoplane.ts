0fcb7097fd82897f78221f5c7741d7951c578c76
AqeeAqee <aqee@msn.com>
2022-05-03 04:32:15 +0800

    add mark/grid interval; remove enum markType; refine block def

--- geoplane.ts
+++ geoplane.ts
@@ -1,16 +1,9 @@
+// dataSerial[], dots/lines
 // color, PrintValue
 // control move with btns
 // f(t)
 // common functions, x, x**2,sin(x), ...
-// data[]
 // drawing methods
-
-enum markType{
-    //% block=Mark
-    mark,
-    //% block=Grid
-    grid,
-}
 
 //% icon="\uf201"
 //% color=#AAAAAA weight=1 
@@ -29,10 +22,10 @@ namespace geoplane {
         }
     }
 
-    let _markType=markType.grid
     let _offsetX = screen.width / 2, _offsetY = screen.height / 2
     let _scale = 10
-    let _colorDefault = 1, _colorPlot = 2, _colorGrid = 11
+    let _colorPlot = 3, _colorMark = 1, _colorGrid = 11, _colorCursor=2
+    let _markInterval=1, _gridInterval=5
 
     let y: number = 0
 
@@ -43,6 +36,7 @@ namespace geoplane {
     //% joinDots.defl=true
     //% blockAllowMultiple=1
     //% blockid=geoplane_drawCurve
+    //% weight=100 group="Basic"
     export function drawCurve(color: number, joinDots: boolean,handler: (x: number) => void) {
         //, joinDots?:boolean , color: number
         // let joinDots = false
@@ -58,35 +52,91 @@ namespace geoplane {
     }
 
     //% block="set Y= |$v"
+    //% weight=99 group="Basic"
     export function setY(v: number) {
         y = v
+    }
+
+    //% block="set origin offset x$offsetX y$offsetY"
+    //% offsetX.defl=80
+    //% offsetY.defl=60
+    //% blockid=geoplane_setOffset
+    //% weight=80 group="Advanced"
+    export function setOffset(offsetX:number,offsetY:number){
+        _offsetX=offsetX
+        _offsetY=offsetY
+    }
+
+    //% block="set scale$scale"
+    //% scale.defl=10
+    //% blockid=geoplane_setScale
+    //% weight=79 group="Advanced"
+    export function setScale(scale: number) {
+        _scale = scale
+    }
+
+    //% block="set Mark color$colorMark=colorindexpicker||, grid color$colorGrid=colorindexpicker, cursor color$colorCursor=colorindexpicker"
+    //% blockid=geoplane_setcolors
+    //% colorMark.defl=1
+    //% colorGrid.defl=11
+    //% colorCursor.defl=2
+    //% weight=0 group="Advanced"
+    //% deprecated blockHidden
+    export function setColors(colorMark: number, colorGrid?: number, colorCursor?: number) {
+        _colorMark = colorMark
+        if (colorGrid) _colorGrid = colorGrid
+        if (colorCursor) _colorCursor = colorCursor
+    }
+
+    //% block="set Mark interval $interval||, color$color=colorindexpicker"
+    //% blockid=geoplane_setmark
+    //% interval.min=0 interval.max=100
+    //% color.defl=1
+    //% weight=70 group="Advanced"
+    export function setMark(interval: number, color?: number) {
+        _markInterval = Math.ceil(interval)
+        if (color) _colorMark = color
+    }
+
+    //% block="set Grid interval $interval||, color$color=colorindexpicker"
+    //% blockid=geoplane_setgrid
+    //% interval.min=0 interval.max=100
+    //% color.defl=11
+    //% weight=69 group="Advanced"
+    export function setGrid(interval: number, color?: number) {
+        _gridInterval = Math.ceil(interval)
+        if (color) _colorGrid = color
     }
 
     game.onPaint(() => {
         if (curves.length < 1) return
 
-        for (let ix = Math.max(0, _offsetX % _scale); ix < screen.width; ix += _scale) {
-            if(_markType==markType.mark)
-                screen.drawLine(ix, _offsetY + 1, ix, _offsetY - 1, _colorDefault)
-            else
+        if (_gridInterval > 0) {
+            for (let ix = _offsetX % (_scale * _gridInterval); ix < screen.width; ix += _scale * _gridInterval) {
                 screen.drawLine(ix, 0, ix, screen.height, _colorGrid)
-        }
-        screen.drawLine(_offsetX, 0, _offsetX, screen.height, _colorDefault)
-
-        for (let iy = Math.max(0, _offsetY % _scale); iy < screen.width; iy += _scale) {
-            if(_markType == markType.mark)
-                screen.drawLine(_offsetX + 1, iy, _offsetX - 1, iy, _colorDefault)
-            else
+            }
+            for (let iy = _offsetY % (_scale * _gridInterval); iy < screen.height; iy += _scale * _gridInterval) {
                 screen.drawLine(0, iy, screen.width, iy, _colorGrid)
+            }
         }
-        screen.drawLine(0, _offsetY, screen.width, _offsetY, _colorDefault)
 
-        for(let cv of curves){
+        if (_markInterval > 0) {
+            for (let ix = _offsetX % (_scale * _markInterval); ix < screen.width; ix += _scale * _markInterval) {
+                screen.drawLine(ix, _offsetY + 1, ix, _offsetY - 1, _colorMark)
+            }
+            for (let iy = _offsetY % (_scale * _markInterval); iy < screen.width; iy += _scale * _markInterval) {
+                screen.drawLine(_offsetX + 1, iy, _offsetX - 1, iy, _colorMark)
+            }
+        }
+        screen.drawLine(_offsetX, 0, _offsetX, screen.height, _colorMark)
+        screen.drawLine(0, _offsetY, screen.width, _offsetY, _colorMark)
+
+        for (let cv of curves) {
             let lastX, lastY
-            y=null
-            for (let x = -_offsetX; x < _offsetX; x += 1/_scale) {
+            y = null
+            for (let x = -_offsetX; x < _offsetX; x += 1 / _scale) {
                 cv.handler(x)
-                if(y==null) continue
+                if (y == null) continue
                 const newX = x * _scale + _offsetX, newY = -y * _scale + _offsetY
                 if (!cv.joinDots)
                     screen.setPixel(newX, newY, cv.color)
@@ -96,39 +146,9 @@ namespace geoplane {
                 // if()  //(newX<0||newX>screen.width||newY<0||newY>screen.height)
                 //     lastX=undefined, lastY=undefined
                 // else
-                    lastX = newX, lastY = newY
+                lastX = newX, lastY = newY
             }
         }
     })
 
-    //% block="set origin offset x$offsetX y$offsetY"
-    //% offsetX.defl=80
-    //% offsetY.defl=60
-    //% blockid=geoplane_setOffset
-    export function setOffset(offsetX:number,offsetY:number){
-        _offsetX=offsetX
-        _offsetY=offsetY
-    }
-
-    //% block="set scale$scale"
-    //% scale.defl=10
-    //% blockid=geoplane_setScale
-    export function setScale(scale: number) {
-        _scale = scale
-    }
-
-    //% block="set scale mark type $v"
-    //% v.defl=markType.mark
-    //% blockid=geoplane_setMarkType
-    export function setMarkType(v: markType) {
-        _markType = v
-    }
-
 }
-
-// geoplane.plot((x) => {
-//     return x * x
-// })
-// geoplane.plot((x) => {
-//     return x
-// })
