91101f7b9b620668b8174dae2c1af72a007cdac0
AqeeAqee <aqee@msn.com>
2022-05-08 04:32:45 +0800

    rename ext; add setOffset(); rename setAxesPos(); add setCanvas()

--- geoplane.ts
+++ geoplane.ts
@@ -1,20 +1,23 @@
-
-// x interval
-// dataSerial[], dots/lines
-// PrintValue
-// control move with btns
-// f(t)
-// common functions, x, x**2,sin(x), ...
+// +extension name: f(x) Graph
+// + set offseted origin, and set position of axes
 // drawing methods
+// + position picker for offsetXY
+// + cache image
+// + x interval
+// ? move cursor with btns , print Y Values at current cursor
+// ? f(t)
+// ? oscillometer
+// demo: dataSerial[], dots/lines
+// demo: common functions, x, x**2,sin(x), ...
 
 //% icon="\uf201"
-//% color=#AAAAAA weight=1 
+//% color=#BBBBBB weight=1 
 //% groups='["Basic", "Advanced"]'
-//% block="Geo Plane"
-namespace geoplane {
+//% block="f(x) Graph"
+namespace functionGraph {
     // type plotFunction = (x: number) => void
-    export let curves: Curve[] = []
-    class Curve {
+    export let functions: GraphFunction[] = []
+    class GraphFunction {
         constructor(
             public handler: (x: number) => void,
             public color: number,
@@ -24,133 +27,217 @@ namespace geoplane {
         }
     }
 
-    let _offsetX = screen.width / 2, _offsetY = screen.height / 2
-    let _scale = 10
-    let _colorPlot = 3, _colorMark = 1, _colorGrid = 11, _colorCursor=2
-    let _markInterval=1, _gridInterval=5
+    let imgCanvas=image.create(screen.width,screen.height)
+    scene.setBackgroundImage(imgCanvas)
 
-    let y: number = 0
+    const AXIS_OFFSET_MARGIN=10
+    let axesPosX = imgCanvas.width / 2, axesPosY = imgCanvas.height / 2
+    let orginOffsetX = 0, orginOffsetY = 0
+    let scale = 10
+    let colorPlot = 2, colorMark = 1, colorGrid = 11, colorCursor=2
+    let markInterval=1, gridInterval=5
 
+    let yValue: number = 0
 
-    //% block="draw curve Y by $x, in color$color=colorindexpicker, join dots $joinDots "
+    /**
+     * Define a graph of a function(x). This will be called many times for each x, as long as x value in the canvas.
+     * Actually, the stepX = 1/scale, one time per pixel on axis X
+     * @param color Color for drawing funciton graph
+     * @param joinDots connect dots(x,y) into lines
+     * @param x the x in f(x), you can grag it into set Y=() block to define your f(x)
+     */
+
+    //% block="graph of y|=|f|(|$x|)|, in color$color=colorindexpicker, join dots $joinDots "
     //% draggableParameters="reporter"
-    //% color.defl=3
+    //% color.defl=2
     //% joinDots.defl=true
     //% blockAllowMultiple=1
-    //% blockid=geoplane_drawCurve
+    //% blockid=functionGraph_addFuctionGraph
     //% weight=100 group="Basic"
-    export function drawCurve(color: number, joinDots: boolean,handler: (x: number) => void) {
+    export function addFuctionGraph(color: number, joinDots: boolean,handler: (x: number) => void) {
         //, joinDots?:boolean , color: number
         // let joinDots = false
         // let color =3
         if (joinDots != true && joinDots !=false) joinDots=true
 
-        if (!color) color = _colorPlot
+        if (!color) color = colorPlot
         color = Math.clamp(1, 15, color)
 
-        const newCurve = new Curve(handler, color, joinDots)
-        if(!curves) curves=[]
-        curves.push(newCurve)
+        const newCurve = new GraphFunction(handler, color, joinDots)
+        if(!functions) functions=[]
+        functions.push(newCurve)
+        redraw()
     }
 
-    //% block="set Y= |$v"
+    /**
+     * Define your f(x), used inside "graph of y=f(x)..." block. 
+     * Add more for each functions.
+     * Drag the "x" from "graph of y=f(x)..." block into your function.
+     * Don't call this, if given "x" out of its domain
+     * @param y the f(x) defination, eg. f(x)=Math.sin(x), f(x)=x*x, f(x)=Math.abs(x-2)
+     */
+    //% block="set f|(|x|) =|$y"
+    //% blockid=functionGraph_setY
     //% weight=99 group="Basic"
-    export function setY(v: number) {
-        y = v
+    export function setY(y: number) {
+        yValue = y
     }
 
-    //% block="set origin offset x$offsetX y$offsetY"
-    //% offsetX.defl=80
-    //% offsetY.defl=60
-    //% blockid=geoplane_setOffset
+    /**
+     * Set offset values of origin point (not screen pixel).
+     */
+    //% block="set origin offset x$x y$y"
+    //% x.defl=0
+    //% y.defl=0
+    //% blockid=functionGraph_setOriginOffset
     //% weight=80 group="Advanced"
-    export function setOffset(offsetX:number,offsetY:number){
-        _offsetX=offsetX
-        _offsetY=offsetY
+    export function setOriginOffset(x: number, y: number) {
+        orginOffsetX = x
+        orginOffsetY = y
+        redraw()
     }
 
-    //% block="set scale$scale"
-    //% scale.defl=10
-    //% blockid=geoplane_setScale
+    /**
+     * Set X axis and Y axis position in canvas, in pixel
+     */
+    //% block="set axes offset x$x y$y"
+    //% x.defl=80
+    //% y.defl=60
+    //% blockid=functionGraph_setAxesPos
+    //% weight=80 group="Advanced"
+    //% x.shadow="positionPicker" y.shadow="positionPicker"
+    export function setAxesPos(x: number, y: number) {
+        axesPosX = x
+        axesPosY = y
+        redraw()
+    }
+
+    //% block="set scale$s"
+    //% s.defl=10
+    //% blockid=functionGraph_setScale
     //% weight=79 group="Advanced"
-    export function setScale(scale: number) {
-        _scale = scale
+    export function setScale(s: number) {
+        scale = s
+        redraw()
     }
 
     //% block="set Mark color$colorMark=colorindexpicker||, grid color$colorGrid=colorindexpicker, cursor color$colorCursor=colorindexpicker"
-    //% blockid=geoplane_setcolors
+    //% blockid=functionGraph_setcolors
     //% colorMark.defl=1
     //% colorGrid.defl=11
     //% colorCursor.defl=2
     //% weight=0 group="Advanced"
     //% deprecated blockHidden
-    export function setColors(colorMark: number, colorGrid?: number, colorCursor?: number) {
-        _colorMark = colorMark
-        if (colorGrid) _colorGrid = colorGrid
-        if (colorCursor) _colorCursor = colorCursor
+    export function setColors(cMark: number, cGrid?: number, cCursor?: number) {
+        colorMark = cMark
+        if (cGrid) colorGrid = cGrid
+        if (cCursor) colorCursor = cCursor
+        redraw()
     }
 
     //% block="set Mark interval $interval||, color$color=colorindexpicker"
-    //% blockid=geoplane_setmark
-    //% interval.min=0 interval.max=100
+    //% blockid=functionGraph_setmark
+    //% interval.defl=1 interval.min=0
     //% color.defl=1
     //% weight=70 group="Advanced"
     export function setMark(interval: number, color?: number) {
-        _markInterval = Math.ceil(interval)
-        if (color) _colorMark = color
+        markInterval = Math.ceil(interval)
+        if (color) colorMark = color
+        redraw()
     }
 
     //% block="set Grid interval $interval||, color$color=colorindexpicker"
-    //% blockid=geoplane_setgrid
-    //% interval.min=0 interval.max=100
+    //% blockid=functionGraph_setgrid
+    //% interval.defl=5 interval.min=0
     //% color.defl=11
     //% weight=69 group="Advanced"
     export function setGrid(interval: number, color?: number) {
-        _gridInterval = Math.ceil(interval)
-        if (color) _colorGrid = color
+        gridInterval = Math.ceil(interval)
+        if (color) colorGrid = color
+        redraw()
     }
 
-    game.onPaint(() => {
-        if (curves.length < 1) return
+    /**
+     * Set canvas for all graphs of funcitons.
+     * Axes positions will be set to the center of the new canvas, automatically. 
+     * @param canvas Image on which all graph of functions drawing on. It been set as scene.BackgroundImage by default for convenience of beginner of this extension.
+     */
+    //% block="set canvas $canvas=background_image_picker"
+    //% blockid=functionGraph_setCanvas
+    //% weight=40 group="Advanced"
+    export function setCanvas(canvas: Image) {
+        if (canvas) {
+            imgCanvas = canvas
+            setAxesPos(canvas.width / 2, canvas.height / 2)
+            // redraw() //called in setAxesPos()
+        }
+    }
 
-        if (_gridInterval > 0) {
-            for (let ix = _offsetX % (_scale * _gridInterval); ix < screen.width; ix += _scale * _gridInterval) {
-                screen.drawLine(ix, 0, ix, screen.height, _colorGrid)
+    /**
+     * Get canvas image
+     */
+    //% block="get canvas image"
+    //% blockid=functionGraph_getCanvas
+    //% weight=39 group="Advanced"
+    export function getCanvas():Image {
+        return imgCanvas
+    }
+
+    /**
+     * Don't need called usually, unless parameters changed, which presents in setY=f(x), outside the "addFuctionGraph(...)".
+     * Blocks, that changing curve parameters, will call this automatically.
+     */
+    //% block="redraw canvas based on all parameters"
+    //% blockid=functionGraph_redraw
+    //% weight=50 group="Advanced"
+    export function redraw() {
+        imgCanvas.fill(15)
+
+        if (gridInterval > 0) {
+            let gridIntervalScaled = Math.ceil(scale * gridInterval)
+            if (gridIntervalScaled < 2) gridIntervalScaled = 2
+            for (let ix = axesPosX % (gridIntervalScaled); ix < imgCanvas.width; ix += gridIntervalScaled) {
+                imgCanvas.drawLine(ix, 0, ix, imgCanvas.height, colorGrid)
             }
-            for (let iy = _offsetY % (_scale * _gridInterval); iy < screen.height; iy += _scale * _gridInterval) {
-                screen.drawLine(0, iy, screen.width, iy, _colorGrid)
+            for (let iy = axesPosY % (gridIntervalScaled); iy < imgCanvas.height; iy += gridIntervalScaled) {
+                imgCanvas.drawLine(0, iy, imgCanvas.width, iy, colorGrid)
             }
         }
 
-        if (_markInterval > 0) {
-            for (let ix = _offsetX % (_scale * _markInterval); ix < screen.width; ix += _scale * _markInterval) {
-                screen.drawLine(ix, _offsetY + 1, ix, _offsetY - 1, _colorMark)
+        if (markInterval > 0) {
+            let markIntervalScaled = Math.ceil(scale * markInterval)
+            if (markIntervalScaled < 2) markIntervalScaled=2
+            for (let ix = axesPosX % (markIntervalScaled); ix < imgCanvas.width; ix += markIntervalScaled) {
+                imgCanvas.drawLine(ix, axesPosY + 1, ix, axesPosY - 1, colorMark)
             }
-            for (let iy = _offsetY % (_scale * _markInterval); iy < screen.width; iy += _scale * _markInterval) {
-                screen.drawLine(_offsetX + 1, iy, _offsetX - 1, iy, _colorMark)
+            for (let iy = axesPosY % (markIntervalScaled); iy < imgCanvas.width; iy += markIntervalScaled) {
+                imgCanvas.drawLine(axesPosX + 1, iy, axesPosX - 1, iy, colorMark)
             }
         }
-        screen.drawLine(_offsetX, 0, _offsetX, screen.height, _colorMark)
-        screen.drawLine(0, _offsetY, screen.width, _offsetY, _colorMark)
 
-        for (let cv of curves) {
+        imgCanvas.drawLine(0, axesPosY, imgCanvas.width, axesPosY, colorMark)
+        imgCanvas.drawLine(axesPosX, 0, axesPosX, imgCanvas.height, colorMark)
+
+        imgCanvas.print("("+orginOffsetX+","+orginOffsetY+")", axesPosX,axesPosY+1,colorMark)
+
+        for (let cv of functions) {
             let lastX, lastY
-            y = null
-            for (let x = -_offsetX; x < _offsetX; x += 1 / _scale) {
+            yValue = null
+            for (let x = -axesPosX / scale +orginOffsetX; x < (160- axesPosX )/ scale+ orginOffsetX; x += 1 / scale) {
                 cv.handler(x)
-                if (y == null) continue
-                const newX = x * _scale + _offsetX, newY = -y * _scale + _offsetY
+                if (yValue == null) continue
+                const newX = (x - orginOffsetX) * scale + axesPosX, newY = -(yValue - orginOffsetY) * scale + axesPosY
                 if (!cv.joinDots)
-                    screen.setPixel(newX, newY, cv.color)
-                else if (!!lastX && !!lastY && !(lastY < 0 && newY > screen.height) && !(newY < 0 && lastY > screen.height)) {
-                    screen.drawLine(lastX, lastY, newX, newY, cv.color)
+                    imgCanvas.setPixel(newX, newY, cv.color)
+                else if (!!lastX && !!lastY && !(lastY < 0 && newY > imgCanvas.height) && !(newY < 0 && lastY > imgCanvas.height)) {
+                    imgCanvas.drawLine(lastX, lastY, newX, newY, cv.color)
                 }
-                // if()  //(newX<0||newX>screen.width||newY<0||newY>screen.height)
+                // if()  //(newX<0||newX>imgCanvas.width||newY<0||newY>imgCanvas.height)
                 //     lastX=undefined, lastY=undefined
                 // else
                 lastX = newX, lastY = newY
             }
         }
-    })
+    }
 
 }
--- main.ts
+++ main.ts
@@ -1,13 +1,31 @@
+game.stats=true
 
+functionGraph.setScale(10)
+functionGraph.setMark(1)
+functionGraph.setGrid(1)
+functionGraph.setOriginOffset(22.5,150)
+functionGraph.setAxesPos(80,60)
 
-// geoplane.setMarkType(markType.mark)
-geoplane.setScale(8)
-// geoplane.setOffset(80,110)
-
-geoplane.drawCurve(3, true,(x) => {
-    geoplane.setY(Math.tan(x))
+functionGraph.addFuctionGraph(3, true,(x) => {
+    functionGraph.setY(Math.tan(x))
 })
 
-geoplane.drawCurve(2, true,(x) => {
-    geoplane.setY(x ** 3)
+functionGraph.addFuctionGraph(2, true,(x) => {
+    functionGraph.setY(x**2)
 })
+
+functionGraph.addFuctionGraph(5, true, function (x) {
+    functionGraph.setY((45 - (x / 3)) * 4)
+})
+functionGraph.addFuctionGraph(5, true, function (x) {
+    functionGraph.setY((42 - (x / 5)) * 4)
+})
+
+// //by default, all ghaph drawn on BackgroundImage
+// //do following if want to draw on a image
+// scene.setBackgroundImage(null)      // or any background image you want
+// let canvas=image.create(150,110)    // any size
+// functionGraph.setCanvas(canvas)          // set new image to be canvas of graph of functions
+// sprites.create(canvas)              // for easy to operate
+
+// functionGraph.canvas.drawRect(10,10,100,100,5)
